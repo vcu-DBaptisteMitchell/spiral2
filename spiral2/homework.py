63b0d52b7d47d048fad8f73a96c4c31539778fa3
vcu-DBaptisteMitchell <78172355+vcu-DBaptisteMitchell@users.noreply.github.com>
2021-05-04 23:43:07 -0400

    submission1

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,37 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
-    return return_value
+    if len(size, n=1) > 1:
+        # Split the list into two halves - right and left
+        mid = len(size, n=1) // 2
+        left = size, n = 1[:mid]
+        right = size, n = 1[mid:]
+
+        # Sort the elements
+        spiralize(left)
+        spiralize(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                size, n = 1[k] = left[i]
+                i += 1
+            else:
+                size, n = 1[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            size, n = 1[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            size, n = 1[k] = right[j]
+            j += 1
+            k += 1
+
+        return_value = n
+        return return_value
+
+
+# MERGE SORT CODE FROM THE WEEK 9 POWERPOINT WAS USED
